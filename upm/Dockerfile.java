a00478e89e3dfe3d067d01d1bb23e33fabca4d80
Nicolas Oliver <dario.n.oliver@intel.com>
2017-10-10 13:24:08 -0700

    upm,mraa: add dockerfiles for upm and mraa images
    Signed-off-by: Nicolas Oliver <dario.n.oliver@intel.com>

---
+++ upm/Dockerfile.java
@@ -0,0 +1,28 @@
+FROM inteliotdevkit/upm-base
+
+# Update apt-get
+RUN apt-get -y update && \
+    # Java Build Dependencies
+    apt-get -y --no-install-recommends install \
+    default-jre default-jdk libmraa-java
+
+# Configure Java Home
+ENV JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64/
+
+# Install Mraa with Java Support
+RUN cd /opt/mraa && \
+  cmake \
+    -DCMAKE_INSTALL_PREFIX=install \
+    -DBUILDSWIG=ON \
+    -DBUILDSWIGPYTHON=OFF \
+    -DBUILDSWIGNODE=OFF \
+    -DBUILDSWIGJAVA=ON \
+    -Bbuild \
+    -H. && \
+  make -Cbuild install && \
+  rm -fr build
+
+# Set Workdir
+WORKDIR $UPM_SRC_DIR
+
+CMD bash
